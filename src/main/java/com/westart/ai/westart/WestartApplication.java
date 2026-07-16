8a47ca09cd10dd33811c2d306efb1a32876d96d0
egoist-ja <3303805462@qq.com>
2026-07-16 10:23:08 +0800

    初始化项目

---
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -0,0 +1,13 @@
+package com.westart.ai.westart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestartApplication.class, args);
+    }
+
+}
